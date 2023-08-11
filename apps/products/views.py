1cf4e56e33517be8139ddcb5602864d1a1eebf8f
nurrative <nursul1997@gmail.com>
2023-08-11 12:12:30 +0600

    added cache

--- apps/products/views.py
+++ apps/products/views.py
@@ -7,16 +7,61 @@ from openpyxl import Workbook
 from django.http import HttpResponse
 from .serializers import ProductSerializer
 from .models import Product
+from django.core.cache import cache
+import redis
+from django.conf import settings
+
+# Подключение к Redis
+redis_client = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 
 class ProductListAPIView(ListAPIView):
     permission_classes = (IsAuthenticated,)
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data, status=200)
-    
 
+    def get_cached_products(self):
+        cached_data = redis_client.get('cached_products')
+        if cached_data:
+            return cached_data
+        else:
+            products = Product.objects.all()
+            serializer = ProductSerializer(products, many=True)
+            serialized_data = serializer.data
+
+            redis_client.setex('cached_products', 7200, serialized_data)
+
+            return serialized_data
+
+    def get(self, request, *args, **kwargs):
+        cached_products = self.get_cached_products()
+        return Response(cached_products, status=200)
+
+    def invalidate_cache(self):
+        redis_client.delete('cached_products')
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 201:
+            self.invalidate_cache()
+
+        return response
+
+    def put(self, request, *args, **kwargs):
+        response = super().put(request, *args, **kwargs)
+
+
+        if response.status_code == 200:
+            self.invalidate_cache()
+
+        return response
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+
+        if response.status_code == 204:
+            self.invalidate_cache()
+
+        return response
 class ProductExportView(APIView):
     permission_classes = (IsAuthenticated,)
     def get(self, request, format=None):
--- config/settings.py
+++ config/settings.py
@@ -144,3 +144,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
